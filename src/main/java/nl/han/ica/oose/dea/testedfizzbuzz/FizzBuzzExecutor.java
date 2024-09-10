d76d833a16ef021adcc3cbc9cbb82e21fb12f2dc
Jae Dreijling <j.dreijling@student.han.nl>
2024-09-10 15:01:19 +0200

    fix main

--- src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
+++ src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
@@ -5,6 +5,14 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzExecutor {
 
     public String execute(int i) {
-        return Integer.toString(i);
+        if (i % 15 == 0) {
+            return "FizzBuzz";
+        } else if (i % 3 == 0) {
+            return "Fizz";
+        } else if (i % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(i);
+        }
     }
 }
